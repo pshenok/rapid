bd0a0b039aa7b73276ae49429394ba99566048dc
Kostyantyn Pshenychnyy <pshenok.dev@gmail.com>
2020-12-14 15:02:40 +0200

    feat: Update service

--- example/src/domain/dog/DogService.ts
+++ example/src/domain/dog/DogService.ts
@@ -1,3 +1,19 @@
+import { IDogRepository, IFindDogInput } from './IDogRepository';
+import { Dog } from './Dog';
+import { IListStructure } from '../domain.types';
+
 export class DogService {
-	
+	constructor (private dogRepository: IDogRepository) {}
+
+	public async createDog (dogData: Partial<Dog>): Promise<Dog> {
+		return this.dogRepository.create(new Dog(dogData));
+	}
+
+	public async getDog (input: IFindDogInput): Promise<Dog | null> {
+		return this.dogRepository.get(input);
+	}
+
+	public async listDogs (input: IFindDogInput): Promise<IListStructure<Dog>> {
+		return this.dogRepository.list(input);
+	}
 }
